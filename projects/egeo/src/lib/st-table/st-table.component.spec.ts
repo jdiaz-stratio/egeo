5ffdda5d13497b3c3799ac64a3831a5808b2c580
jdiaz-stratio <jdiaz@stratio.com>
2020-03-02 11:45:35 +0100

    remove dependencies

--- projects/egeo/src/lib/st-table/st-table.component.spec.ts
+++ projects/egeo/src/lib/st-table/st-table.component.spec.ts
@@ -13,8 +13,6 @@ import { ChangeDetectionStrategy } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import * as _ from 'lodash';
-import { fromEvent, Observable } from 'rxjs';
-import { map, share } from 'rxjs/operators';
 
 import { Order, ORDER_TYPE } from './shared/order';
 import { StTableComponent } from './st-table.component';
